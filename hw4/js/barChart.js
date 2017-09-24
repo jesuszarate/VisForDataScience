ddfead6a4df1088919903c0440aa32a9ff8686c7
Jesus <chuy8jay@gmail.com>
2017-09-23 18:39:07 -0600

    X axis spaced by fives

--- hw4/js/barChart.js
+++ hw4/js/barChart.js
@@ -36,15 +36,16 @@ class BarChart {
         let width = parseInt(svg.attr("width"));
         let height = parseInt(svg.attr("height"));
 
-        this.axes('#xAxis', selectedDimension, width, height, this.allData);
+        this.Yaxis('#xAxis', selectedDimension, width, height, this.allData);
+        this.Xaxis('#yAxis', 'year', width, height, this.allData);
 
         // Create the bars (hint: use #bars)
 
         let aScale = d3.scaleLinear()
             .domain([0, d3.max(this.allData, d => d[selectedDimension])])
-            .range([0, width]);
+            .range([0, height]);
 
-        this.updateABarGraphWithData('#bars', aScale, this.allData);
+        this.updateABarGraphWithData('#bars', selectedDimension, aScale, this.allData, width, height);
 
 
         // ******* TODO: PART II *******
@@ -76,31 +77,76 @@ class BarChart {
     }
 
     Xaxis(idName, dataName, width, height, data) {
+        let svg = d3.select(idName);
 
+        let padding = 100;
+
+        //let spacing = height / data.length;
+
+        let min = d3.min(data, function (d) {
+            return d[dataName];
+        });
+        let max = d3.max(data, function (d) {
+            return d[dataName];
+        });
+
+        let ticks = this.getYears(data);
+
+        console.log("min: " + min);
+        console.log("max: " + max);
+
+        let xScale = d3.scaleTime()
+            .domain([new Date(min, 0, 1), new Date(max, 0, 1)])
+            //.domain([min, max])
+            .range([0, width - padding + 20])
+            //.rangeRound([0,width - padding + 20], 100)
+             .nice();
+
+        // let xScale = d3.scaleLinear()
+        //     .domain([min, max])
+        //     //.range([0, width - padding + 20])
+        //     .rangeRound([0,width - padding + 20], 10)
+        //     .nice();
+
+        // let color = d3.scaleLinear()
+        //     .domain([min, max])
+        //     .range(["darkred", "lightgray", "steelblue"]);
+
+        // let ticks = data.filter(function (d) {
+        //     return d.year;
+        // });
+
+        console.log(ticks.length);
+
+        let xAxis = d3.axisBottom();
+        xAxis.scale(xScale)
+            .ticks(d3.timeYear.every(4))
+            .ticks(20)
+            //.tickPadding([10])
+            //.tickValues(ticks)
+            .tickFormat(d3.timeFormat("%Y"));
+
+
+        // css class for the axis
+        svg.classed("axis", true)
+        //.attr("transform", "translate(" + 75 + "," + 355 + ")")
+            .attr("transform", "translate(" + 75 + "," + 355 + ")")
+            .call(xAxis)
+            .selectAll("text")
+            .attr("y", 0)
+            .attr("x", 10)
+            //.attr("dy", ".35em")
+            .attr("dy", ".35em")
+            .attr("transform", "rotate(90)")
+            .style("text-anchor", "start");
     }
 
     Yaxis(idName, dataName, width, height, data) {
         let svg = d3.select(idName);
 
-        let padding = 25;
+        let padding = 45;
 
-        //let width = parseInt(svg.attr("width"));
-        //let height = parseInt(svg.attr("height"));
-
-        //execute = function () {
-        //let data = [0.3, -0.8, 0.01, -0.4, 1.2, 1.3, -0.2, -1.21, 0.82, 0.4, -0.2, 0.3];
-
-
-        // Convert the data array to an object, so that it's easy to look up
-        // data values by state names
-        // let dataLookup = {};
-        // data.forEach(function (stateRow) {
-        //     // d3.csv will read the values as strings; we need to convert them to floats
-        //     let so = stateRow[dataName];
-        //     dataLookup[dataName] = parseFloat(stateRow[dataName]);
-        // });
-
-        let spacing = height / data.length;
+        // let spacing = height / data.length;
 
         let min = d3.min(data, function (d) {
             return d[dataName];
@@ -111,54 +157,75 @@ class BarChart {
 
         console.log("min: " + min);
         console.log("max: " + max);
-        let xScale = d3.scaleLinear()
-            .domain([0, 200])
-            .range([0, width])
+        let yScale = d3.scaleLinear()
+            .domain([max, min])
+            .range([10, height - padding])
             .nice();
-
-        console.log(width);
 
         // let color = d3.scaleLinear()
         //     .domain([min, max])
         //     .range(["darkred", "lightgray", "steelblue"]);
 
-        let xAxis = d3.axisLeft();
-        xAxis.scale(xScale);
+        let yAxis = d3.axisLeft();
+        yAxis.scale(yScale);
 
         // css class for the axis
         svg.classed("axis", true)
-            // moving the axis to the right place
-            .attr("transform", "translate(" + 50 + "," + (padding) + ")")
-            .call(xAxis);
+            .attr("transform", "translate(" + 75 + "," + 0 + ")")
+            .call(yAxis);
     }
 
-    updateABarGraphWithData(idName, scale, data) {
+    updateABarGraphWithData(idName, dataName, scale, data, width, height) {
 
+        //data = [70, 100, 30];
 
-        let data_name = 'goals';
+        //let data_name = 'goals';
         let svg = d3.select(idName);
 
-        let width = parseInt(svg.attr("width"));
-        let height = parseInt(svg.attr("height"));
-
         // add padding on all sides
-        let padding = 0;
+        let padding = 75;
+        let spacing = width / data.length;
+
+        let min = d3.min(data, function (d) {
+            return d[dataName];
+        });
+        let max = d3.max(data, function (d) {
+            return d[dataName];
+        });
+
+        let aScale = d3.scaleLinear()
+            .domain([min, max])
+            //.range([height - padding, 10]);
+            //.rangeRound([10,height - padding + 20], 10);
+            .range([10, height - padding + 20]);
+        //.range([0, height]);
 
         // svg.attr({
         //     width: width + 2 * padding,
         //     height: height + 2 * padding
         // });
 
+        let barWidth = (width - padding) / data.length;
+
+        data = BarChart.sortData(data, dataName);
+
         let bars = svg.selectAll("rect")
             .data(data);
 
         let newBars = bars.enter().append("rect")
-        // .attr("x", function (d, i) {
-        //     return i * 10;
-        // })
-            .attr("y", 0)
-            .attr("width", 20)
-            .attr("height", 0)
+            .attr("x", function (d, i) {
+                return (i * barWidth) + padding;
+            })
+            .attr("y", function (d, i) {
+                let nd = aScale(d[dataName]);
+                return (height - nd - 45);
+            })
+            .attr("width", barWidth - 5)
+            //.attr("width", barWidth)
+            .attr("height", function (d) {
+                let nd = d[dataName];
+                return aScale(nd);
+            })
             .style("opacity", 0)
             .classed("barChart", true);
 
@@ -174,22 +241,41 @@ class BarChart {
         bars.transition()
             .duration(3000)
             .attr("x", function (d, i) {
-                return i * 10;
+                return (i * barWidth) + padding;
             })
-            .attr("y", 0)
-            .attr("width", 10)
+            .attr("y", function (d, i) {
+                let nd = aScale(d[dataName]);
+                return (height - nd - 45);
+            })
+            .attr("width", barWidth - 5)
+            //.attr("width", barWidth)
             .attr("height", function (d) {
-                return scale(d[data_name]);
+                let nd = d[dataName];
+                return aScale(nd);
             })
             .style("fill", "steelblue")
             .style("opacity", 1);
+    }
 
+    getYears(data) {
+        let values = [];
+        for (let i = 0, len = data.length; i < len; i++) {
+            values.push(new Date(data[i].year, 0, 1));
+            //values.push(data[i].year);
+        }
+        return values;
+    };
 
-        /////////////////////////
-
-
-        ////////////////////////
-
+    static sortData(data, dataName) {
+        data = data.sort(function compare(a, b) {
+            if (a['year'] < b['year'])
+                return -1;
+            else if (a['year'] > b['year'])
+                return 1;
+            else
+                return 0;
+        });
+        return data;
     }
 
 }
