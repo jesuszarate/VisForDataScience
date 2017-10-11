c5759bdae948e962e4f601007807c2d67c07304f
Jesus <jesus.zarate.eng@gmail.com>
2017-10-11 17:00:23 -0600

    Have a linear color scale being displayed correctly as well as the number with the bar graphs. Also displaying the text right.

--- hw5/js/table.js
+++ hw5/js/table.js
@@ -6,14 +6,14 @@ class Table {
     constructor(teamData, treeObject) {
 
         //Maintain reference to the tree Object; 
-        this.tree = null; 
+        this.tree = treeObject;
 
         // Create list of all elements that will populate the table
         // Initially, the tableElements will be identical to the teamData
-        this.tableElements = null; // 
+        this.tableElements = teamData.slice(); //
 
         ///** Store all match data for the 2014 Fifa cup */
-        this.teamData = null;
+        this.teamData = teamData;
 
         //Default values for the Table Headers
         this.tableHeaders = ["Delta Goals", "Result", "Wins", "Losses", "TotalGames"];
@@ -33,18 +33,48 @@ class Table {
         this.goalsMadeHeader = 'Goals Made';
         this.goalsConcededHeader = 'Goals Conceded';
 
+
+        let goals = [];
+        goals.push(d3.max(this.teamData, d => d.value[this.goalsConcededHeader]));
+        goals.push(d3.max(this.teamData, d => d.value[this.goalsMadeHeader]));
+
         /** Setup the scales*/
-        this.goalScale = null; 
+        this.goalScale = d3.scaleLinear()
+            .domain([0, d3.max(goals, d => d)])
+            .range([this.cell.buffer, this.cell.width * 2 - this.cell.buffer]);
+
 
         /** Used for games/wins/losses*/
-        this.gameScale = null; 
+        this.gameScale = null;
 
         /**Color scales*/
         /**For aggregate columns  Use colors '#ece2f0', '#016450' for the range.*/
-        this.aggregateColorScale = null; 
+        this.aggregateColorScale = d3.scaleLinear()
+            .range([
+                "#ece2f0",
+                '#016450'
+            ])
+            // ([
+            //     "rgb(203, 208, 217)",
+            //     "rgb(172, 189, 194)",
+            //     "rgb(109, 149, 145)",
+            //     "rgb(112, 153, 149)",
+            //     "rgb(76, 123, 115)",
+            //     "rgb(57, 115, 102)",
+            //     "rgb(41, 97, 79)"])
+            .domain([
+                d3.min(this.tableElements, function (d) {
+                    //console.log(d.value["Wins"]);
+                    return d.value["Wins"];
+                }),
+                d3.max(this.tableElements, function (d) {
+                    //console.log(d.value["Wins"]);
+                    return d.value["Wins"];
+                })
+            ]);
 
         /**For goal Column. Use colors '#cb181d', '#034e7b'  for the range.*/
-        this.goalColorScale = null; 
+        this.goalColorScale = null;
     }
 
 
@@ -72,7 +102,7 @@ class Table {
 
         // Clicking on headers should also trigger collapseList() and updateTable(). 
 
-       
+
     }
 
 
@@ -82,15 +112,108 @@ class Table {
     updateTable() {
         // ******* TODO: PART III *******
         //Create table rows
+        let tr = d3.select("tbody").selectAll("tr")
+            .data(this.tableElements)
+            .enter()
+            .append("tr");
 
         //Append th elements for the Team Names
+        let th = tr.selectAll("th").data(function (d) {
+            return [d.key];
+        })
+            .enter()
+            .append("th")
+            .text(function (d) {
+                return d;
+            });
 
-        //Append td elements for the remaining columns. 
+        //Append td elements for the remaining columns.
         //Data for each cell is of the type: {'type':<'game' or 'aggregate'>, 'value':<[array of 1 or two elements]>}
-        
+        let goalsMadeHeader = 'Goals Made';
+        let goalsConcededHeader = 'Goals Conceded';
+
+        let td = tr.selectAll("td").data(function (d) {
+            /* create data array here */
+            console.log(d);
+            return [
+                //{'type': d.value['aggregate'], 'vis': 'text', 'value': d.key},
+                {
+                    'type': d.value['aggregate'],
+                    'vis': 'goals',
+                    'value': [d.value[goalsMadeHeader], d.value[goalsConcededHeader], d.value["Delta Goals"]]
+                },
+                {'type': d.value['aggregate'], 'vis': 'text', 'value': [d.value["Result"].label]},
+                {'type': d.value['aggregate'], 'vis': 'bar', 'value': [d.value["Wins"]]},
+                {'type': d.value['aggregate'], 'vis': 'bar', 'value': [d.value["Losses"]]},
+                {'type': d.value['aggregate'], 'vis': 'bar', 'value': [d.value["TotalGames"]]}
+            ];
+        })
+            .enter()
+            .append('td');
+
+
         //Add scores as title property to appear on hover
 
         //Populate cells (do one type of cell at a time )
+
+
+
+        // Population of bar cells
+        let bars = td.filter(function (d) {
+            return d.vis === 'bar';
+        })
+            .append("svg")
+            .attr("width", this.cell.width + 20)
+            .attr("height", this.cell.height + 5);
+
+
+        let colorScale = this.aggregateColorScale;
+        let rect = bars.append("rect")
+            .attr("height", 20)
+            .attr("width", function (d) {
+                return d.value[0] * 10;
+            })
+            .style("fill", function (d) {
+                return colorScale(d.value[0]);
+            });
+        //.classed("aggregate", true);
+
+
+        let text = bars
+            .append("text")
+            .text(function (d) {
+                return d.value[0];
+            })
+            // We need to move the label to the middle of the slice. Our arc generator
+            // is smart enough to know how to do this. Notice that arc.centroid gives us the center of the visible wedge.
+            .attr("transform", function (d) {
+                console.log(d.value[0] * 10 - 10);
+                return "translate(" + (d.value[0] * 10  - 5) + "," + 10 + ")";
+            })
+            // Finally some extra text styling to make it look nice:
+            .attr("dy", ".35em")
+            .style("fill", "white")
+            .style("text-anchor", "middle")
+            .style("font-size", "10px");
+
+
+        // let rectText = bars.append("text")
+        //     .text(function (d) {
+        //         console.log(d.value[0]);
+        //         return d.value[0];
+        //     })
+        //     .attr("transform", function (d) {
+        //         return "translate(" + 0  + "," + 0 + ")";
+        //     });
+
+        // Population of text cells
+        td.filter(function (d) {
+            return d.vis === 'text';
+        })
+            .text(function (d) {
+                return d.value;
+            });
+
 
         //Create diagrams in the goals column
 
@@ -104,9 +227,9 @@ class Table {
      */
     updateList(i) {
         // ******* TODO: PART IV *******
-       
+
         //Only update list for aggregate clicks, not game clicks
-        
+
     }
 
     /**
@@ -114,42 +237,24 @@ class Table {
      *
      */
     collapseList() {
-        
+
         // ******* TODO: PART IV *******
 
     }
 
 
-
-    Xaxis(idName, dataName, width, height, data) {
+    Xaxis(idName, dataName, width, height, data, scale) {
         let svg = d3.select(idName)
             .append("svg")
-             .attr("width", width * 2)
-             .attr("height", height * 2);
-
-        let padding = 45;
-
-        // let min = d3.min(data, function (d) {
-        //     return d[dataName];
-        // });
-        // let max = d3.max(data, function (d) {
-        //     return d[dataName];
-        // });
-
-        let min = 0;
-        let max = 5;
-
-        let xScale = d3.scaleLinear()
-            .domain([min, max])
-            .range([0, 100])
-            .nice();
+            .attr("width", width * 2 + 20)
+            .attr("height", height);
 
         let xAxis = d3.axisBottom();
-        xAxis.scale(xScale);
+        xAxis.scale(this.goalScale);
 
         // css class for the axis
         svg.classed("axis", true)
-            .attr("transform", "translate(" + 0 + "," + 10 + ")")
+            .attr("transform", "translate(" + 0 + "," + 0 + ")")
             .call(xAxis);
     }
 
