38bf0bd20176cf4ddeb3c934e19ff57383767f01
Jesus <jesus.zarate.eng@gmail.com>
2017-10-28 01:07:36 -0600

    init commit for hw6

--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-git pull origin https://github.com/dataviscourse/2017-dataviscourse-homework.git 
+git fetch https://github.com/dataviscourse/2017-dataviscourse-homework.git
+git checkout FETCH_HEAD -- hw6
