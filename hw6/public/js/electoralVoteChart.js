5dd902c946eb7c7d030416adfadc0f8d5d28664b
Jesus <jesus.zarate.eng@gmail.com>
2017-10-28 20:54:52 -0600

    Completed part I

--- hw6/public/js/electoralVoteChart.js
+++ hw6/public/js/electoralVoteChart.js
@@ -1,13 +1,12 @@
-   
 class ElectoralVoteChart {
     /**
      * Constructor for the ElectoralVoteChart
      *
      * @param shiftChart an instance of the ShiftChart class
      */
-    constructor (shiftChart){
+    constructor(shiftChart) {
         this.shiftChart = shiftChart;
-        
+
         this.margin = {top: 30, right: 20, bottom: 30, left: 50};
         let divelectoralVotes = d3.select("#electoral-vote").classed("content", true);
 
@@ -18,8 +17,8 @@ class ElectoralVoteChart {
 
         //creates svg element within the div
         this.svg = divelectoralVotes.append("svg")
-            .attr("width",this.svgWidth)
-            .attr("height",this.svgHeight)
+            .attr("width", this.svgWidth)
+            .attr("height", this.svgHeight)
 
     };
 
@@ -28,14 +27,14 @@ class ElectoralVoteChart {
      *
      * @param party an ID for the party that is being referred to.
      */
-    chooseClass (party) {
-        if (party == "R"){
+    chooseClass(party) {
+        if (party == "R") {
             return "republican";
         }
-        else if (party == "D"){
+        else if (party == "D") {
             return "democrat";
         }
-        else if (party == "I"){
+        else if (party == "I") {
             return "independent";
         }
     }
@@ -48,39 +47,67 @@ class ElectoralVoteChart {
      * @param colorScale global quantile scale based on the winning margin between republicans and democrats
      */
 
-   update (electionResult, colorScale){
+    update(electionResult, colorScale) {
 
-          // ******* TODO: PART II *******
+        // ******* TODO: PART II *******
 
-    //Group the states based on the winning party for the state;
-    //then sort them based on the margin of victory
+        //Group the states based on the winning party for the state;
+        //then sort them based on the margin of victory
 
-    //Create the stacked bar chart.
-    //Use the global color scale to color code the rectangles.
-    //HINT: Use .electoralVotes class to style your bars.
+        // Grouping by winning party
 
-    //Display total count of electoral votes won by the Democrat and Republican party
-    //on top of the corresponding groups of bars.
-    //HINT: Use the .electoralVoteText class to style your text elements;  Use this in combination with
-    // chooseClass to get a color based on the party wherever necessary
 
-    //Display a bar with minimal width in the center of the bar chart to indicate the 50% mark
-    //HINT: Use .middlePoint class to style this bar.
+        let electionResultsByParty = d3.nest()
+            .key(function (d) {
+                return d["State_Winner"];
+            })
+            .entries(electionResult);
 
-    //Just above this, display the text mentioning the total number of electoral votes required
-    // to win the elections throughout the country
-    //HINT: Use .electoralVotesNote class to style this text element
+        console.log(electionResultsByParty);
 
-    //HINT: Use the chooseClass method to style your elements based on party wherever necessary.
+        let cleanResults = [];
 
-    //******* TODO: PART V *******
-    //Implement brush on the bar chart created above.
-    //Implement a call back method to handle the brush end event.
-    //Call the update method of shiftChart and pass the data corresponding to brush selection.
-    //HINT: Use the .brush class to style the brush.
+        for (let prty in electionResultsByParty) {
+            cleanResults.concat(electionResultsByParty[prty].values)
+        }
+
+        for (let prty in cleanResults) {
+
+            cleanResults[prty].sort(function (x, y) {
+                return d3.ascending(x["RD_Difference"], y["RD_Difference"]);
+            });
+        }
+
+        console.log(electionResultsByParty);
+
+        this.svg.selectAll(".rec")
+
+        //Create the stacked bar chart.
+        //Use the global color scale to color code the rectangles.
+        //HINT: Use .electoralVotes class to style your bars.
+
+        //Display total count of electoral votes won by the Democrat and Republican party
+        //on top of the corresponding groups of bars.
+        //HINT: Use the .electoralVoteText class to style your text elements;  Use this in combination with
+        // chooseClass to get a color based on the party wherever necessary
+
+        //Display a bar with minimal width in the center of the bar chart to indicate the 50% mark
+        //HINT: Use .middlePoint class to style this bar.
+
+        //Just above this, display the text mentioning the total number of electoral votes required
+        // to win the elections throughout the country
+        //HINT: Use .electoralVotesNote class to style this text element
+
+        //HINT: Use the chooseClass method to style your elements based on party wherever necessary.
+
+        //******* TODO: PART V *******
+        //Implement brush on the bar chart created above.
+        //Implement a call back method to handle the brush end event.
+        //Call the update method of shiftChart and pass the data corresponding to brush selection.
+        //HINT: Use the .brush class to style the brush.
 
 
     };
 
-    
+
 }
--- hw6/public/js/yearChart.js
+++ hw6/public/js/yearChart.js
@@ -1,4 +1,3 @@
-
 class YearChart {
 
     /**
@@ -10,7 +9,7 @@ class YearChart {
      * @param electionInfo instance of ElectionInfo
      * @param electionWinners data corresponding to the winning parties over mutiple election years
      */
-    constructor (electoralVoteChart, tileChart, votePercentageChart, electionWinners) {
+    constructor(electoralVoteChart, tileChart, votePercentageChart, electionWinners) {
 
         //Creating YearChart instance
         this.electoralVoteChart = electoralVoteChart;
@@ -18,7 +17,7 @@ class YearChart {
         this.votePercentageChart = votePercentageChart;
         // the data
         this.electionWinners = electionWinners;
-        
+
         // Initializes the svg elements required for this chart
         this.margin = {top: 10, right: 20, bottom: 30, left: 50};
         let divyearChart = d3.select("#year-chart").classed("fullView", true);
@@ -31,7 +30,13 @@ class YearChart {
         //add the svg to the div
         this.svg = divyearChart.append("svg")
             .attr("width", this.svgWidth)
-            .attr("height", this.svgHeight)
+            .attr("height", this.svgHeight);
+
+        /** Setup the scales*/
+        this.yearScale = d3.scaleLinear()
+            .range([50, this.svgWidth - 50])
+            //.range([this.cell.buffer, this.cell.width * 2 - this.cell.buffer]);
+            .domain([1940, 2012]);
     };
 
 
@@ -40,7 +45,7 @@ class YearChart {
      *
      * @param party an ID for the party that is being referred to.
      */
-    chooseClass (data) {
+    chooseClass(data) {
         if (data == "R") {
             return "yearChart republican";
         }
@@ -53,48 +58,144 @@ class YearChart {
     }
 
     /**
-     * Creates a chart with circles representing each election year, populates text content and other required elements for the Year Chart
+     * Creates a chart with circles representing each election year, populates text content and other required elements
+     * for the Year Chart
      */
-    update () {
+    update() {
 
         //Domain definition for global color scale
         let domain = [-60, -50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50, 60];
 
         //Color range for global color scale
-        let range = ["#063e78", "#08519c", "#3182bd", "#6baed6", "#9ecae1", "#c6dbef", "#fcbba1", "#fc9272", "#fb6a4a", "#de2d26", "#a50f15", "#860308"];
+        let range = ["#063e78", "#08519c", "#3182bd", "#6baed6", "#9ecae1", "#c6dbef", "#fcbba1", "#fc9272", "#fb6a4a",
+            "#de2d26", "#a50f15", "#860308"];
 
         //ColorScale be used consistently by all the charts
         this.colorScale = d3.scaleQuantile()
             .domain(domain)
             .range(range);
 
-       // ******* TODO: PART I *******
+        // ******* TODO: PART I *******
 
-    // Create the chart by adding circle elements representing each election year
-    //The circles should be colored based on the winning party for that year
-    //HINT: Use the .yearChart class to style your circle elements
-    //HINT: Use the chooseClass method to choose the color corresponding to the winning party.
+        // Create the chart by adding circle elements representing each election year
+        //The circles should be colored based on the winning party for that year
+        //HINT: Use the .yearChart class to style your circle elements
+        //HINT: Use the chooseClass method to choose the color corresponding to the winning party.
+        let circleRadius = 25;
+        let reference = this;
 
-    //Append text information of each year right below the corresponding circle
-    //HINT: Use .yeartext class to style your text elements
+        let node = this.svg.selectAll(".node")
+            .data(this.electionWinners)
+            .enter().append("g")
+            .attr("transform", function (d) {
+                return "translate(" + reference.yearScale(d["YEAR"]) + "," + (circleRadius + 5) + ")";
+            });
 
-    //Style the chart by adding a dashed line that connects all these years.
-    //HINT: Use .lineChart to style this dashed line
-
-    //Clicking on any specific year should highlight that circle and  update the rest of the visualizations
-    //HINT: Use .highlighted class to style the highlighted circle
-
-    //Election information corresponding to that year should be loaded and passed to
-    // the update methods of other visualizations
+        //Append text information of each year right below the corresponding circle
+        //HINT: Use .yeartext class to style your text elements
+        // adds the text to the node
 
 
-    //******* TODO: EXTRA CREDIT *******
+        //Style the chart by adding a dashed line that connects all these years.
+        //HINT: Use .lineChart to style this dashed line
+        this.renderLines(this.svg, this.yearScale, circleRadius);
+        this.renderNodes(node, this.yearScale, circleRadius);
 
-    //Implement brush on the year chart created above.
-    //Implement a call back method to handle the brush end event.
-    //Call the update method of shiftChart and pass the data corresponding to brush selection.
-    //HINT: Use the .brush class to style the brush.
+        //line = newLine.merge(line);
+
+
+        //Clicking on any specific year should highlight that circle and  update the rest
+        // of the visualizations
+        //HINT: Use .highlighted class to style the highlighted circle
+
+        //Election information corresponding to that year should be loaded and passed to
+        // the update methods of other visualizations
+
+
+        //******* TODO: EXTRA CREDIT *******
+
+        //Implement brush on the year chart created above.
+        //Implement a call back method to handle the brush end event.
+        //Call the update method of shiftChart and pass the data corresponding to brush selection.
+        //HINT: Use the .brush class to style the brush.
 
     };
 
-};
+    renderNodes(node, scale, radius, colorScale) {
+        let reference = this;
+        node.append("circle")
+            .attr("r", function (d) {
+                return radius;
+            })
+            .attr("class", function (d) {
+                return reference.chooseClass(d["PARTY"])
+            })
+            .on("click", function (d) {
+                //let data = reference.loadElectionResults(d["YEAR"]);
+                reference.nodeSelected(d["YEAR"], reference);
+
+                d3.csv("data/year_timeline_" + d["YEAR"] + ".csv", function (error, yearTimeline) {
+                    reference.electoralVoteChart.update(yearTimeline, colorScale)
+                });
+            })
+            .on("mouseover", function (d) {
+                reference.nodeMouseOver(d["YEAR"], reference);
+            });
+
+        node.append("text")
+            .attr("dy", "2.5em")
+            .attr("class", "yeartext")
+            .text(function (d) {
+                return d["YEAR"];
+            });
+    }
+
+    loadElectionResults(year) {
+        //load the data corresponding to all the election years
+        //pass this data and instances of all the charts that update on year selection to yearChart's constructor
+        d3.csv("data/year_timeline_" + year + ".csv", function (error, yearTimeline) {
+
+        });
+    }
+
+
+    nodeMouseOver(year, reference) {
+        let node = this.svg.selectAll(".node")
+            .data(this.electionWinners)
+            .enter();
+        node.selectAll("circle")
+            .attr("class", function (d) {
+                let clss = reference.chooseClass(d["PARTY"]);
+                return d["YEAR"] === year ? clss + " highlighted" : clss;
+            });
+    }
+
+    nodeSelected(year, reference) {
+        let node = this.svg.selectAll(".node")
+            .data(this.electionWinners)
+            .enter();
+        node.selectAll("circle")
+            .attr("class", function (d) {
+                let clss = reference.chooseClass(d["PARTY"]);
+                return d["YEAR"] === year ? clss + " selected" : clss;
+            });
+    }
+
+    renderLines(svg, scale, radius) {
+
+        svg.selectAll(".line")
+            .data(this.electionWinners)
+            .enter()
+            .append("line")
+            .attr("class", "lineChart")
+            .attr("x1", function (d) {
+                return scale(+d['YEAR']) + radius;
+            })
+            .attr("y1", radius + 5)
+            .attr("x2", function (d) {
+                return scale(+d['YEAR'] + 4) - radius;
+            })
+            .attr("y2", radius + 5);
+    }
+
+}
